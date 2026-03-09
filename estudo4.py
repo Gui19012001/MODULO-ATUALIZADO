8cd39ea1b8516617ebd46294ad536d68c1476a23
Gui19012001 <guilherme.cruz@iberoequipamentos.com.br>
2026-03-09 16:11:05 -0300

    Update estudo4.py

--- estudo4.py
+++ estudo4.py
@@ -493,8 +493,8 @@ def pagina_apontamento():
     st.markdown("#  Registrar Apontamento")
     st.markdown("### ⏱️ Produção Hora a Hora")
 
-    OP_TIMEOUT_SEG = 10
-    RESET_TIMEOUT_SEG = 10
+    OP_TIMEOUT_SEG = 15
+    RESET_TIMEOUT_SEG = 15
 
     @st.cache_data(ttl=15)
     def carregar_apontamentos_cache():
